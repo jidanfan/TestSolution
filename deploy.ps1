97ac3057d55a8b6c120057e371d47b81d5f58c62
jidanfan <jidanfan@gmail.com>
2023-02-25 19:29:57 +0800

    uuu

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,18 @@
 $ErrorActionPreference = 'Stop';
 
-docker ps 
+ Write-Host  $env:APPVEYOR_REPO_TAG_NAME
+
+    $env:APPVEYOR_REPO_TAG_NAME
+
+    Write-Host  ${env:APPVEYOR_REPO_TAG_NAME}
+
+    ${env:APPVEYOR_REPO_TAG_NAME}
+
+    if ($env:APPVEYOR_REPO_TAG_NAME)
+    {
+      docker ps
+    }
+    else
+    {
+      docker images 
+    }
