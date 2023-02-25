3add9ae5ad197b4d5ed9724f56f2e8a436263430
jidanfan <jidanfan@gmail.com>
2023-02-25 19:35:35 +0800

    echo

--- deploy.ps1
+++ deploy.ps1
@@ -1,12 +1,12 @@
 $ErrorActionPreference = 'Stop';
 
- Write-Host  $env:APPVEYOR_REPO_TAG_NAME
+echo  $env:APPVEYOR_REPO_TAG_NAME
 
-    $env:APPVEYOR_REPO_TAG_NAME
+$env:APPVEYOR_REPO_TAG_NAME
 
-    Write-Host  ${env:APPVEYOR_REPO_TAG_NAME}
+echo  ${env:APPVEYOR_REPO_TAG_NAME}
 
-    ${env:APPVEYOR_REPO_TAG_NAME}
+${env:APPVEYOR_REPO_TAG_NAME}
 
     if ($env:APPVEYOR_REPO_TAG_NAME)
     {
