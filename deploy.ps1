5d3dabb48492accbb0cab452c412f534a24ebeab
jidanfan <jidanfan@gmail.com>
2023-02-25 21:35:53 +0800

    ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,16 +1,23 @@
 $ErrorActionPreference = 'Stop';
+"Hello world"
+
+"INFO: $APPVEYOR_REPO_TAG"
+
+"INFO: tag name: $Env:APPVEYOR_REPO_TAG_NAME"
+
+"INFO: $APPVEYOR_REPO_TAG_NAME"
 
 echo  "$env:APPVEYOR_REPO_TAG_NAME"
 
 echo  "${env:APPVEYOR_REPO_TAG_NAME}"
 
-${env:APPVEYOR_REPO_TAG_NAME}
+"${env:APPVEYOR_REPO_TAG_NAME}"
 
-    if ($env:APPVEYOR_REPO_TAG_NAME)
-    {
-      docker ps
-    }
-    else
-    {
-      docker images 
+if ($env:APPVEYOR_REPO_TAG_NAME)
+{
+   docker ps
+ }
+ else
+ {
+    docker images 
     }
